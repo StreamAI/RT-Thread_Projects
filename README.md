e4613c9a664d2e733af35c5751b39707d7f3d4dc
StreamAI <wang.baoliang@qq.com>
2019-09-10 00:44:04 +0800

    Add rt-thread porting project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # RT-Thread_Projects
 RT-Thread_STM32L475
+
+
+
+### 1.《[IOT-OS之RT-Thread（一）---系统启动与移植过程](https://blog.csdn.net/m0_37621078/article/details/100584591)》
+
---
+++ projects/stm32l4xx/applications/main.c
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-11-06     SummerGift   first version
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <board.h>
+
+/* defined the LED_R/LED_G/LED_B pin: PE7/PE8/PE9 */
+#define LED_R    GET_PIN(E, 7)
+#define LED_G    GET_PIN(E, 8)
+#define LED_B    GET_PIN(E, 9)
+
+/* defined RGB LED Color enum */
+typedef enum{
+	RED,
+	GREEN,
+	BLUE,
+	YELLOW,
+	PURPLE,
+	CYAN,
+	WHITE,
+	BLACK,
+	MAX_NUM
+}RGB_Color;
+
+
+int RGB_LED_init(void)
+{
+    /* set LED_R/LED_G/LED_B pin mode to output */
+    rt_pin_mode(LED_R, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED_G, PIN_MODE_OUTPUT);
+    rt_pin_mode(LED_B, PIN_MODE_OUTPUT);
+
+    return 0;
+}
+
+INIT_APP_EXPORT(RGB_LED_init);
+
+void RGB_ON(RGB_Color RGB_LED)
+{
+	switch(RGB_LED % MAX_NUM)
+		{
+			case RED:
+				rt_pin_write(LED_R, PIN_LOW);
+				rt_pin_write(LED_G, PIN_HIGH);
+				rt_pin_write(LED_B, PIN_HIGH);
+        		rt_kprintf("red led on.\n");
+				break;
+			case GREEN:
+				rt_pin_write(LED_R, PIN_HIGH);
+				rt_pin_write(LED_G, PIN_LOW);
+				rt_pin_write(LED_B, PIN_HIGH);
+        		rt_kprintf("green led on.\n");
+				break;
+			case BLUE:
+				rt_pin_write(LED_R, PIN_HIGH);
+				rt_pin_write(LED_G, PIN_HIGH);
+				rt_pin_write(LED_B, PIN_LOW);
+        		rt_kprintf("blue led on.\n");
+				break;
+			case YELLOW:
+				rt_pin_write(LED_R, PIN_LOW);
+				rt_pin_write(LED_G, PIN_LOW);
+				rt_pin_write(LED_B, PIN_HIGH);
+        		rt_kprintf("yellow led on.\n");
+				break;
+			case PURPLE:
+				rt_pin_write(LED_R, PIN_LOW);
+				rt_pin_write(LED_G, PIN_HIGH);
+				rt_pin_write(LED_B, PIN_LOW);
+        		rt_kprintf("purple led on.\n");
+				break;
+			case CYAN:
+				rt_pin_write(LED_R, PIN_HIGH);
+				rt_pin_write(LED_G, PIN_LOW);
+				rt_pin_write(LED_B, PIN_LOW);
+        		rt_kprintf("cyan led on.\n");
+				break;
+			case WHITE:
+				rt_pin_write(LED_R, PIN_LOW);
+				rt_pin_write(LED_G, PIN_LOW);
+				rt_pin_write(LED_B, PIN_LOW);
+        		rt_kprintf("white led on.\n");
+				break;
+			default:
+				rt_pin_write(LED_R, PIN_HIGH);
+				rt_pin_write(LED_G, PIN_HIGH);
+				rt_pin_write(LED_B, PIN_HIGH);
+        		rt_kprintf("led off.\n");
+				break;
+		}
+}
+
+int main(void)
+{
+    unsigned int count = 0;
+
+    while (count < 64)
+    {
+        RGB_ON(count);
+        rt_thread_mdelay(1000);
+		count++;
+    }
+
+    return RT_EOK;
+}
